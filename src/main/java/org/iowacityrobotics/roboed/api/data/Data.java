8bae438e538237784b8c09331a3661bc37cd73bc
phantamanta44 <evandalong@gmail.com>
2016-11-02 17:24:24 -0500

    More utility data pipeline methods

--- src/main/java/org/iowacityrobotics/roboed/api/data/Data.java
+++ src/main/java/org/iowacityrobotics/roboed/api/data/Data.java
@@ -2,6 +2,7 @@ package org.iowacityrobotics.roboed.api.data;
 
 import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 /**
  * Utility class for working with data streams.
@@ -14,7 +15,7 @@ public class Data {
      * @param src1 The first pipeline.
      * @param src2 The second pipeline.
      * @param mapper The data interpolation function.
-     * @return The pipelines with the new interpolating segment.
+     * @return The pipeline with the new interpolating segment.
      */
     public static <I1, I2, O> IDataSource<O> interpolate(IDataSource<I1> src1, IDataSource<I2> src2, BiFunction<I1, I2, O> mapper) {
         return new InterpolatingPipeline<>(src1, src2, mapper);
@@ -22,9 +23,8 @@ public class Data {
     
     /**
      * The implementation of {@link Data#interpolate(IDataSource, IDataSource, BiFunction) interpolate()}.
-     * @author Evan Geng
      */
-    static class InterpolatingPipeline<I1, I2, O> implements IDataSource<O> {
+    private static class InterpolatingPipeline<I1, I2, O> implements IDataSource<O> {
 
         private final IDataSource<I1> src1;
         private final IDataSource<I2> src2;
@@ -52,6 +52,48 @@ public class Data {
             return null;
         }
         
+    }
+
+    /**
+     * Creates a pipeline that's fed from a {@link Supplier}.
+     * @param provider The supplier feeding this pipeline.
+     * @param <T> The type of data being passed.
+     * @return The new pipeline.
+     */
+    public static <T> IDataSource<T> provider(Supplier<T> provider) {
+        return new ProviderPipeline<>(provider);
+    }
+
+    /**
+     * Creates a pipeline that always provides a constant value.
+     * @param value The value to be provided.
+     * @param <T> The type of data being passed.
+     * @return The new pipeline.
+     */
+    public static <T> IDataSource<T> constant(T value) {
+        return provider(() -> value);
+    }
+
+    /**
+     * The implementation of {@link Data#provider(Supplier) provider()}.
+     */
+    private static class ProviderPipeline<T> implements IDataSource<T> {
+
+        private final Supplier<T> provider;
+
+        ProviderPipeline(Supplier<T> provider) {
+            this.provider = provider;
+        }
+
+        @Override
+        public <V> IDataSource<V> map(Function<T, V> mapper) {
+            return new MappingPipeline<>(this, mapper);
+        }
+
+        @Override
+        public T request() {
+            return provider.get();
+        }
     }
     
 }
