3304b80fcc3a1c97bc9016033f94a0884f896ab7
phantamanta44 <evandalong@gmail.com>
2016-11-03 14:43:35 -0500

    Suppress serial version id warning

--- src/main/java/org/iowacityrobotics/roboed/api/operations/OperationInterruptor.java
+++ src/main/java/org/iowacityrobotics/roboed/api/operations/OperationInterruptor.java
@@ -3,4 +3,5 @@ package org.iowacityrobotics.roboed.api.operations;
 /**
  * Exception to interrupt the execution of an opmode.
  */
+@SuppressWarnings("serial")
 public class OperationInterruptor extends RuntimeException { }
