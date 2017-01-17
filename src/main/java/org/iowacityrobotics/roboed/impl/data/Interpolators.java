9eb1cc7e2b2c479d29eacafb42a009bf38157ad5
phantamanta44 <evandalong@gmail.com>
2017-01-16 21:43:26 -0600

    Dual joystick throttling interpolator

--- src/main/java/org/iowacityrobotics/roboed/impl/data/Interpolators.java
+++ src/main/java/org/iowacityrobotics/roboed/impl/data/Interpolators.java
@@ -3,19 +3,17 @@ package org.iowacityrobotics.roboed.impl.data;
 import org.iowacityrobotics.roboed.util.collection.Pair;
 import org.iowacityrobotics.roboed.util.math.Vector2;
 
-import java.util.function.DoubleSupplier;
-import java.util.function.Function;
+import java.util.function.BiFunction;
 
 /**
  * @author Evan Geng
  */
 public class Interpolators {
 
-    public static Function<Pair<Vector2, Vector2>, Pair<Vector2, Vector2>> throttle(DoubleSupplier throttleFunc) {
-        return p -> {
-            double throttle = throttleFunc.getAsDouble();
-            p.getA().x(p.getA().x() * throttle).y(p.getA().y() * throttle);
-            p.getB().x(p.getB().x() * throttle).y(p.getB().y() * throttle);
+    public static BiFunction<Pair<Vector2, Vector2>, Double, Pair<Vector2, Vector2>> throttle() {
+        return (p, t) -> {
+            p.getA().x(p.getA().x() * t).y(p.getA().y() * t);
+            p.getB().x(p.getB().x() * t).y(p.getB().y() * t);
             return p;
         };
     }
